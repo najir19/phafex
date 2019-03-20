c448674fb64d32dae45b550fd69f65caaea7105b
Najir Ahamed <31666616+najir19@users.noreply.github.com>
2019-03-20 16:09:46 +0600

    Phafex psd

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+ "use strict";
+$(document).ready(function(){
+ 	$('.testimonial').slick({
+ 		autoplay:true,
+ 		arrows:true,
+ 		prevArrow: '<span class="prev-arrow"><i class="fa fa-arrow-left"></i></span>',
+        nextArrow: '<span class="next-arrow"><i class="fa fa-arrow-right"></i></span>',
+ 	});
+});
+
+})(jQuery); 
